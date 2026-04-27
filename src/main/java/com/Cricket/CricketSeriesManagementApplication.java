4e1bef838e0211cd563c4d375e29af249bdbe87a
Atharva-Shelke <atharva.a.s.00@gmail.com>
2026-04-27 23:53:09 +0530

    Restructured project

---
+++ src/main/java/com/Cricket/CricketSeriesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.Cricket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CricketSeriesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CricketSeriesManagementApplication.class, args);
+	}
+
+}
