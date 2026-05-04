866394e589d8c0c2a16d8ef66b5105c0f9051572
Atharva-Shelke <atharva.a.s.00@gmail.com>
2026-05-04 20:01:44 +0530

    Changed return type from String to ResponseEntity to return map of messages and other data for all methods.

--- src/main/java/com/cricketSeries/controller/CricketController.java
+++ src/main/java/com/cricketSeries/controller/CricketController.java
@@ -1,9 +1,9 @@
 package com.cricketSeries.controller;
 
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,11 +15,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.cricketSeries.model.Series;
-import com.cricketSeries.dto.MatchDTO;
+import com.cricketSeries.dto.MatchRequestDTO;
+import com.cricketSeries.dto.MatchResponseDTO;
 import com.cricketSeries.dto.SeriesRequestDTO;
 import com.cricketSeries.dto.SeriesResponseDTO;
-import com.cricketSeries.model.Match;
 import com.cricketSeries.service.CricketService;
 
 @RestController
@@ -30,14 +29,19 @@ public class CricketController {
 	private CricketService service;
 
 	@PostMapping("/series")
-	public ResponseEntity<String> create(@RequestBody SeriesRequestDTO series) {
+	public ResponseEntity<Map<String, String>> create(@RequestBody SeriesRequestDTO series) {
 		service.createSeries(series);
-		return ResponseEntity.status(HttpStatus.CREATED).body("Created");
+		return ResponseEntity.ok(Map.of("message", "Series created successfully"));
 	}
 
 	@GetMapping("/series")
-	public List<Series> getAllSeries() {
+	public List<SeriesResponseDTO> getAllSeries() {
 		return service.getAllSeries();
+	}
+
+	@GetMapping("/seriesWithMatches")
+	public List<SeriesResponseDTO> getAllSeriesWithMatches() {
+		return service.getAllSeriesWithMatches();
 	}
 
 	@GetMapping("/series/{id}")
@@ -45,44 +49,51 @@ public class CricketController {
 		return service.getSeriesById(id);
 	}
 
-	@DeleteMapping("/series")
-	public String deleteSeries(@RequestParam Long id) {
-		service.deleteSeries(id);
-		return "Series deleted with id: " + id;
+	@GetMapping("/seriesWithMatches/{id}")
+	public SeriesResponseDTO getSeriesByIdWithMatches(@PathVariable Long id) {
+		return service.getSeriesByIdWithMatches(id);
 	}
 
-	@PutMapping("/series")
-	public String updateSeries(@RequestBody Series series) {
-		service.updateSeries(series);
-		return "Series updated with id: " + series.getId();
+	@DeleteMapping("/series")
+	public ResponseEntity<Map<String, String>> deleteSeries(@RequestParam Long id) {
+		service.deleteSeries(id);
+		return ResponseEntity.ok(Map.of("message", "Series deleted successfully"));
+	}
+
+	@PutMapping("/series/{id}")
+	public ResponseEntity<Map<String, String>> update(@PathVariable Long id, @RequestBody SeriesRequestDTO dto) {
+
+		service.updateSeries(id, dto);
+
+		return ResponseEntity.ok(Map.of("message", "Series updated successfully"));
 	}
 
 	@PostMapping("/match/{id}")
-	public ResponseEntity<String> createMatch(@PathVariable Long id, @RequestBody MatchDTO match) {
+	public ResponseEntity<Map<String, String>> createMatch(@PathVariable Long id, @RequestBody MatchRequestDTO match) {
 		service.createMatch(id, match);
-		return ResponseEntity.status(HttpStatus.CREATED).body("Created");
+		return ResponseEntity.ok(Map.of("message", "Match created successfully in series " + id));
 	}
 
 	@GetMapping("/matches/{id}")
-	public List<MatchDTO> getAllMatchesBySeriesId(@PathVariable Long id) {
+	public List<MatchResponseDTO> getAllMatchesBySeriesId(@PathVariable Long id) {
 		return service.getAllMatchesBySeriesId(id);
 	}
 
 	@GetMapping("/match/{id}")
-	public MatchDTO getMatchById(@PathVariable Long id) {
+	public MatchResponseDTO getMatchById(@PathVariable Long id) {
 		return service.getMatchById(id);
 	}
 
 	@DeleteMapping("/match")
-	public String deleteMatch(@RequestParam Long id) {
+	public ResponseEntity<Map<String, String>> deleteMatch(@RequestParam Long id) {
 		service.deleteMatch(id);
-		return "Match deleted with id: " + id;
+		return ResponseEntity.ok(Map.of("message", "Match id " + id + " deleted successfully"));
 	}
 
 	@PutMapping("/match/{id}")
-	public String updateMatch(@PathVariable Long id, @RequestBody MatchDTO match) {
+	public ResponseEntity<Map<String, String>> updateMatch(@PathVariable Long id, @RequestBody MatchRequestDTO match) {
 		service.updateMatch(id, match);
-		return "Match updated with id: " + match.getId();
+		return ResponseEntity.ok(Map.of("message", "Match id " + id + " updated successfully"));
 	}
 
 }
